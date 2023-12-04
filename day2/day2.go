265c9a936d32332cae75c6e2079b779150d3aa2e
stubz151 <stubz151@gmail.com>
2023-12-04 20:40:10 +0200

    feat: day 2 p2

--- day2/day2.go
+++ day2/day2.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 )
 
-func main() {
+func partOne() {
 	f, err := os.Open("input")
 	if err != nil {
 		log.Fatal(err)
@@ -46,7 +46,6 @@ func main() {
 					cubeMap[word] = remaining
 				} else {
 					possible = false
-					fmt.Printf("\n position %d word %s", i, word)
 					break
 				}
 			}
@@ -56,4 +55,48 @@ func main() {
 		}
 	}
 	fmt.Println(total)
+}
+
+func partTwo() {
+	f, err := os.Open("input")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err = f.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	scanner := bufio.NewScanner(f)
+	total := 0
+	regSet := regexp.MustCompile(`\; \b`)
+	regPull := regexp.MustCompile(`\b, \b`)
+
+	for i := 0; scanner.Scan(); i++ {
+		line := scanner.Text()
+		subSets := regSet.Split(line, -1)
+		cubeMap := map[string]int{"red": 0, "green": 0, "blue": 0}
+		for _, curSet := range subSets {
+			subStrings := regPull.Split(curSet, -1)
+			for _, curWord := range subStrings {
+				split := strings.Split(curWord, " ")
+				number, _ := strconv.Atoi(string(split[0]))
+				word := split[1]
+
+				remaining := cubeMap[word]
+				if number > remaining {
+					cubeMap[word] = number
+				}
+			}
+		}
+		total += cubeMap["red"] * cubeMap["green"] * cubeMap["blue"]
+	}
+	fmt.Println(total)
+}
+
+func main() {
+	fmt.Print("p1: ")
+	partOne()
+	fmt.Print("p2: ")
+	partTwo()
 }
